6ebc9306b253a339668133340c50ebd009842099
gigmelepcha <gigmelepcha9@gmail.com>
2025-03-26 22:23:13 +0530

    add exception for invalid inputs and handle for None

--- string_calculator.py
+++ string_calculator.py
@@ -1,10 +1,21 @@
+class InvalidInputException(Exception):
+    """Custom exception for invalid inputs."""
+    pass
+
 class StringCalculator:
     @staticmethod
     def add(numbers):
-        if numbers == "":
+        if numbers is None: # None input
             return 0
-        elif "," not in numbers:  # single number
+        if numbers == "": # Empty string
+            return 0
+        elif "," not in numbers:    # Single number
+            if not numbers.isdigit():  # Check for alphabetic characters
+                raise InvalidInputException("Invalid inputs: Charaters are not allowed")
             return int(numbers)
         else:
             num_list = numbers.split(",") # multiple numbers seperated by comma
+            # Check for alphabetic characters after splitting
+            if any(any(char.isalpha() for char in num) for num in num_list):
+                raise InvalidInputException("Invalid inputs: Charaters are not allowed")
             return sum(int(num) for num in num_list)
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -1,9 +1,11 @@
 import unittest
 from string_calculator import StringCalculator
+from string_calculator import InvalidInputException
 
 class TestStringCalculator(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(StringCalculator.add(''), 0)
+
     def test_single_number(self):
         self.assertEqual(StringCalculator.add('1'), 1)
         self.assertEqual(StringCalculator.add('5'), 5)
@@ -16,6 +18,24 @@ class TestStringCalculator(unittest.TestCase):
         self.assertEqual(StringCalculator.add('1,2,3'), 6)
         self.assertEqual(StringCalculator.add('10,20,30,40'), 100)
 
+    def test_none_input(self):
+        self.assertEqual(StringCalculator.add(None), 0)
+
+    def test_invalid_inputs(self):
+        # Test input with alphabets
+        with self.assertRaises(InvalidInputException) as context:
+            StringCalculator.add("1,a,3")
+        self.assertEqual(str(context.exception), "Invalid inputs: Charaters are not allowed")
+
+        # Test input with mixed alphabets and numbers
+        with self.assertRaises(InvalidInputException) as context:
+            StringCalculator.add("1asd")
+        self.assertEqual(str(context.exception), "Invalid inputs: Charaters are not allowed")
+
+        # Test input with only alphabets
+        with self.assertRaises(InvalidInputException) as context:
+            StringCalculator.add("abc")
+        self.assertEqual(str(context.exception), "Invalid inputs: Charaters are not allowed")
 
 if __name__ == '__main__':
     unittest.main()
