4d10cae9e0f953e444f331d59497b79f676704c0
gigmelepcha <gigmelepcha9@gmail.com>
2025-04-08 18:18:18 +0530

    made the code more modular by make the parse and checnking negative a seperate methods

--- string_calculator.py
+++ string_calculator.py
@@ -4,24 +4,20 @@ class InvalidInputException(Exception):
 
 class StringCalculator:
     @staticmethod
-    def add(numbers):
-         """
-        Adds numbers provided in a string format, supporting custom delimiters and validations.
+    def parseString(numbers):
+        """
+        Parses the input string into a list of numbers based on the delimiter.
 
         Parameters:
-        - numbers (str): A string containing numbers separated by delimiters.
+        - numbers (str): The input string containing numbers.
 
         Returns:
-        - int: The sum of the numbers in the string.
-
-        Raises:
-        - InvalidInputException: If the input contains invalid characters, non-numeric values, or negative numbers.
-
+        - list: A list of parsed numbers as strings.
         """
-        if numbers is None:  # None input
+        if numbers is None or numbers =="":  # None input
             return 0
-        if numbers == "":  # Empty string
-            return 0
+        # if numbers == "":  # Empty string
+        #     return 0
 
         # Handle custom delimiter
         delimiter = ","
@@ -47,7 +43,9 @@ class StringCalculator:
                 current_number += char
         if current_number:  # Add the last number
             num_list.append(current_number)
-
+        return num_list
+    @staticmethod
+    def HandleNegative(num_list):
         # Check for negative numbers
         negatives = [num for num in num_list if num.strip().startswith("-") and num.strip()[1:].isdigit()]
         if negatives:
@@ -55,5 +53,25 @@ class StringCalculator:
         # Check for invalid characters (alphabetic or non-numeric)
         if any(any(not char.isdigit() for char in num.strip()) for num in num_list):
             raise InvalidInputException("Invalid inputs: Characters or non-numeric values are not allowed")    # Filter out empty strings and convert valid numbers to integers
+     
+    @staticmethod
+    def add(numbers):
+        """
+        Adds numbers provided in a string format, supporting custom delimiters and validations.
+
+        Parameters:
+        - numbers (str): A string containing numbers separated by delimiters.
+
+        Returns:
+        - int: The sum of the numbers in the string.
+
+        Raises:
+        - InvalidInputException: If the input contains invalid characters, non-numeric values, or negative numbers.
+
+        """
+        num_list = StringCalculator.parseString(numbers)
+        if num_list == 0:
+            return 0
+        StringCalculator.HandleNegative(num_list)
         valid_numbers = [int(num) for num in num_list if num.strip().isdigit()]
         return sum(valid_numbers)
